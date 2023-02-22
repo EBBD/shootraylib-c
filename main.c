f7434af51941fa06972816f75c8361ef8c1b39ac
EBBD <109923078+EBBD@users.noreply.github.com>
2023-02-22 12:24:22 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,48 @@
 #include "raylib.h"
 
-int main(void)
-{
-  const int screenWidth = 800;
-  const int screenHeight = 450;
+int main(void){
+  const int screenWidth = 450;
+  const int screenHeight = 800;
+  int i = 0;
 
   InitWindow(screenWidth, screenHeight, "shootGame");
   Vector2 shipPosition = { (float)screenWidth/2, (float)screenHeight/2 };
 
   SetTargetFPS(60);
   
-   while (!WindowShouldClose())
-   {
-     if (IsKeyDown(KEY_A)) ballPosition.x += 4.0f;
-     if (IsKeyDown(KEY_D)) ballPosition.x -= 4.0f;
-     if (IsKeyDown(KEY_S)) ballPosition.y += 4.0f;
-     if (IsKeyDown(KEY_W)) ballPosition.y -= 4.0f;
-     
+   while (!WindowShouldClose()){
+     if (shipPosition.x < 25){
+       shipPosition.x = 25;
+     }
+     else if (shipPosition.x > 425){
+       shipPosition.x = 425;
+     }
+     else if (shipPosition.y < 25){
+       shipPosition.y = 25;
+     }
+     else if (shipPosition.y > 775){
+       shipPosition.y = 775;
+     }
+     else{
+      if (IsKeyDown(KEY_D)) shipPosition.x += 2.0f;
+      if (IsKeyDown(KEY_A)) shipPosition.x -= 2.0f;
+      if (IsKeyDown(KEY_S)) shipPosition.y += 2.0f;
+      if (IsKeyDown(KEY_W)) shipPosition.y -= 2.0f;
+   }
+     if (i == 0){
      BeginDrawing();
-            ClearBackground(RAYBLACK);
+            ClearBackground(BLACK);
 
             DrawText("move the ball with arrow keys", 10, 10, 20, WHITE);
 
-            DrawCircleV(ballPosition, 25, MAROON);
+            DrawCircleV(shipPosition, 25, MAROON);
 
      EndDrawing();
+       i++;
+     }
+     else{
+       i = 0;
+     }
    }
      
      CloseWindow();
