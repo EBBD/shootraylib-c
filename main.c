6360c15be19858182f515659eb07ccf19b2ded49
EBBD <109923078+EBBD@users.noreply.github.com>
2023-02-28 12:33:01 -0500

    Update main.c
    to do
    - fix shooting bug
    - actually add textures

--- main.c
+++ main.c
@@ -4,9 +4,10 @@ int main(void){
   const int screenWidth = 450;
   const int screenHeight = 800;
   int i = 0;
+  int j = 0;
   int lockout = 0;
   int playerShots[5];
-  int shotPos[5][2];
+  float shotPos[5][2];
   int shotHolder = 0;
 
   InitWindow(screenWidth, screenHeight, "shootGame");
@@ -41,11 +42,26 @@ int main(void){
         if (IsKeyDown(KEY_W)) {
           shipPosition.y -= 2.0f;
         }
-	if (IsKeyDown(KEY_SPACE)) {
-	  playerShot[shotHolder] == 1;
-	  shotPos[shotHolder][0] == shipPosition.x;
-	  shotPos[shotHolder][1] == shipPosition.y - 5.0f;
-	  shotHolder++;
+	if (IsKeyPressed(KEY_SPACE) && shotHolder < 5) {
+	  playerShots[shotHolder] = 1;
+	  shotPos[shotHolder][0] = shipPosition.x;
+	  shotPos[shotHolder][1] = shipPosition.y - 10.0f;
+	  if (playerShots[0] == 0){
+	shotHolder = 0;
+	  }
+else if (playerShots[1] == 0){
+	shotHolder = 1;
+	  }
+else if (playerShots[2] == 0){
+	shotHolder = 2;
+	  }
+else if (playerShots[3] == 0){
+	shotHolder = 3;
+	  }
+else if (playerShots[4] == 0){
+	shotHolder = 4;
+	  }
+
 	}
       }
    }
@@ -57,14 +73,24 @@ int main(void){
 
             DrawCircleV(shipPosition, 25, MAROON);
 	    
-
+	    while (playerShots[j] != 0){
+		    DrawRectangle((int)shotPos[j][0], (int)shotPos[j][1], 10, 10, YELLOW);
+		    shotPos[j][1] -= 5.0f;
+		    if(shotPos[j][1] <= 0.0f){
+			    playerShots[j] = 0;
+			    shotHolder = j;
+			    
+		    }
+		    j++;
+	    }
+            j = 0;        
      EndDrawing();
        i++;
      }
      else{
        i = 0;
      }
-   }
+   }  
      
      CloseWindow();
   
